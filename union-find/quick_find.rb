70e5bfae5dff9e5830d5992f13f25544985af804
Awin Abi <awinabi@gmail.com>
2013-09-24 17:30:53 +0530

    quick find

--- union-find/quick_find.rb
+++ union-find/quick_find.rb
@@ -1,19 +1,36 @@
 class QuickFind
 
-  def initialize
+  def initialize(n)
+    @qf_ary = *(0..n.to_i)
   end
     
-  def union
+  def union(a,b)
+    if @qf_ary[a] != @qf_ary[b]
+      @qf_ary.each do |i|
+        if @qf_ary[i] == @qf_ary[a]
+          @qf_ary[i] = @qf_ary[b]
+        end
+      end
+    end
   end
 
-  def is_connected?
+  def is_connected?(a,b)
+    @qf_ary[a] == @qf_ary[b]
   end
 
-  def execute
-    puts "executing..."
+  def print_state
+    puts @qf_ary.to_s
   end
 
 end
 
-qf = QuickFind.new
-qf.execute()
+qf = QuickFind.new(10)
+qf.print_state()
+qf.union(0,1)
+qf.print_state()
+qf.union(0,5)
+qf.print_state()
+qf.union(2,4)
+qf.print_state()
+puts "connected(1,5) => #{qf.is_connected?(1,5)}"
+

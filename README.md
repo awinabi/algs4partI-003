9db3a3f4bf1e1d484b95ffcb7e14c4388afa967d
Awin Abi <awinabi@gmail.com>
2013-09-24 17:07:32 +0530

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ algs4partI-003
 ==============
 
 Algorithms, Part I - Coursera Exercises
+[Course Page](https://class.coursera.org/algs4partI-003/class)
---
+++ union-find/quick_find.rb
@@ -0,0 +1,19 @@
+class QuickFind
+
+  def initialize
+  end
+    
+  def union
+  end
+
+  def is_connected?
+  end
+
+  def execute
+    puts "executing..."
+  end
+
+end
+
+qf = QuickFind.new
+qf.execute()
